fea76fd8de7cd43c3ec97d17f27f15c2f109c2fe
aashrayasachdeva <hello@aashsach.me>
2023-11-09 15:30:41 +0530

    base commit

--- pages/chatbot.py
+++ pages/chatbot.py
@@ -7,7 +7,7 @@ import openai
 st.set_page_config(page_title="MU ML Tutor", page_icon="🧑‍🏫")
 st.title("MU ML Tutor Chatbot 🧑‍🏫")
 
-openai.api_key = st.secrets.openai.api_key
+openai.api_key = st.session_state.get("openai_key", None)
 
 if "messages" not in st.session_state:
     st.session_state["messages"] = []
@@ -33,34 +33,39 @@ if prompt := st.chat_input():
     st.session_state.messages.append({"role": "user", "content": prompt})
     st.chat_message("user").write(prompt)
     with st.spinner("generating response..."):
-        message = openai.beta.threads.messages.create(
-            thread_id=thread_id,
-            role="user",
-            content=prompt,
-        )
-
-        run = openai.beta.threads.runs.create(
-            thread_id=thread_id,
-            assistant_id=st.secrets.openai.assistant_id,
-        )
-
-        total_time = 0
-        while (run_status := openai.beta.threads.runs.retrieve(thread_id=thread_id,
-                                                              run_id=run.id).status) not in ["cancelled", "failed", "completed", "expired"]:
-            total_time += 2
-            time.sleep(2)
-
-            if total_time > 10:
-                break
-
-        if run_status == "completed":
-            response = openai.beta.threads.messages.list(
-                thread_id=thread_id
+        try:
+            message = openai.beta.threads.messages.create(
+                thread_id=thread_id,
+                role="user",
+                content=prompt,
             )
-            response = response.data[0]
-            for r in response.content:
-                st.session_state.messages.append({"role": "assistant", "content": r.text.value})
-                st.chat_message("assistant").write(r.text.value)
-        else:
-            st.chat_message("assistant").write("oops something went wrong, please try again!")
+
+            run = openai.beta.threads.runs.create(
+                thread_id=thread_id,
+                assistant_id=st.secrets.openai.assistant_id,
+            )
+
+            total_time = 0
+            while (run_status := openai.beta.threads.runs.retrieve(thread_id=thread_id,
+                                                                  run_id=run.id).status) not in ["cancelled", "failed", "completed", "expired"]:
+                total_time += 2
+                time.sleep(2)
+
+                if total_time > 10:
+                    break
+
+            if run_status == "completed":
+                response = openai.beta.threads.messages.list(
+                    thread_id=thread_id
+                )
+                response = response.data[0]
+                for r in response.content:
+                    st.session_state.messages.append({"role": "assistant", "content": r.text.value})
+                    st.chat_message("assistant").write(r.text.value)
+            else:
+                st.chat_message("assistant").write("oops something went wrong, please try again!")
+        except openai.AuthenticationError:
+            st.error("Invalid OpenAI API, please refresh page and login with valid openai key.")
+        except Exception as err:
+            st.error(err)
 
--- streamlit_app.py
+++ streamlit_app.py
@@ -14,12 +14,19 @@ if "auth" not in st.session_state:
 if not st.session_state["auth"]:
     username = st.text_input("username")
     password = st.text_input("password", type="password")
-    auth = True
+    openai_key = st.text_input("openAI key", type="password")
+
 
     for valid_username, valid_password in st.secrets.credentials.users:
         if valid_username == username and valid_password == password:
+            if openai_key:
+                st.session_state["openai_key"] = openai_key
+            else:
+                if valid_username == "admin" :
+                    st.session_state["openai_key"] = st.secrets.openai.api_key
+                else:
+                    st.stop()
             st.session_state["auth"] = True
-            break
 
 
 if st.session_state["auth"]:
